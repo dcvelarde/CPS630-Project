2095ab0b5945e9653151d2e446b360169ac296b2
velarded <danica.cristine.velarde@gmail.com>
2019-03-24 18:51:37 -0400

    added recipe search functionality

--- Project/scripts/recipes-controller.js
+++ Project/scripts/recipes-controller.js
@@ -3,20 +3,20 @@ angular.module('recipeModule', [])
 
     function RecipeController($scope,$http) {
         var vm = this;
-        $scope.recipeHeading = "Recipes App";
-        $scope.change = false;
-        $scope.changeRecipeHeading = changeRecipeHeading;
+        $scope.recipeHeading = "Foodgether";
         $scope.searchForRecipes = searchForRecipes;
+        $scope.couldNotFindAnyResults = false;
         $scope.listOfRecipes = [];
+        $scope.dietFilters = [];
+        $scope.healthFilters = [];
+        $scope.dietFilterOptions = ["balanced","high-protein","high-fiber","low-fat","low-carb",
+        "low-sodium"];
+        $scope.healthFilterOptions = ["vegan",
+        "vegetarian","paleo","dairy-free","gluten-free","wheat-free","fat-free","low-sugar",
+        "egg-free","peanut-free","tree-nut-free","soy-free","fish-free","shellfish-free"];
+
         var appID = "4e6ed2f0";
         var appKey = "2b62e270b8ccede3c8380b07051800a6";
-        function changeRecipeHeading() {
-            $scope.change = !$scope.change;
-            if($scope.change)
-                $scope.recipeHeading = "CPS630 Recipe Project";
-            else
-                $scope.recipeHeading = "Recipes App";
-        }
 
         $scope.getRequest = function() {
             console.log("I've been pressed!");
@@ -31,14 +31,28 @@ angular.module('recipeModule', [])
             );
           };
 
-        function searchForRecipes(queryIngredients) {
+        function searchForRecipes(queryIngredients, dietFilters, healthFilters) {
             console.log(queryIngredients);
-            console.log("Get first 10 recipes for a given ingredient");
-            $http.get("https://api.edamam.com/search?q="+queryIngredients+"&app_id="+appID+"&app_key="+appKey+"&to=10").then(
+            var dietFilterParams = "";
+            for(var i = 0; i <dietFilters.length; i++){
+              dietFilterParams = dietFilterParams+"&diet="+dietFilters[i];
+            }
+
+            var healthFilterParams = "";
+            for(var i = 0; i <healthFilters.length; i++){
+              healthFilterParams = healthFilterParams+"&health="+healthFilters[i];
+            }
+            console.log("Get first 10 recipes for given ingredient(s)");
+            $http.get("https://api.edamam.com/search?q="+queryIngredients+dietFilterParams+healthFilterParams
+              +"&app_id="+appID+"&app_key="+appKey+"&to=10").then(
               function successCallback(response) {
                 $scope.response = response;
                 console.log(response);
                 $scope.listOfRecipes = response.data.hits;
+                if($scope.listOfRecipes.length != 0)
+                  $scope.couldNotFindAnyResults = false;
+                else
+                  $scope.couldNotFindAnyResults = true;
               },
               function errorCallback(response) {
                 console.log("Unable to perform get request");
